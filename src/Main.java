91c2c22760b7a7225ff2895aecaa16c94f49aeba
Nick <nicklost5555@gmail.com>
2023-10-30 21:14:36 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i<=5; i++) {
+            System.out.println("");
+            for (int j = 1; j<=5; j++) {
+                System.out.print("*");
+            }
+        }
+
+        System.out.println(" ");
+
+        for (int i = 1; i<=5; i++) {
+            System.out.println("");
+            for (int j = i; j<=5; j++) {
+                System.out.print("*");
+            }
+        }
+
+        System.out.println(" ");
+
+        for (int i = 1; i<=5; i++) {
+            System.out.println("");
+            for (int j = 1; j<=i; j++) {
+                System.out.print("*");
+            }
+        }
+    }
+}
